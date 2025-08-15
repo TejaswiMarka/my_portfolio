c2298c62c246286ee377ccf604d4d47523f4be1a
Marka Tejaswi <tejaswimarka07@gmail.com>
2025-08-15 09:38:45 +0530

    tejupotfolio
    this is my portfolio

---
+++ tejupotfolio/portfolio/js/script.js
@@ -0,0 +1,7 @@
+/*============typing animation========*/
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer" ," uipath certified developer "," devops developer"],
+    typeSpeed:50,
+    BackSpeed:25,
+    loop:true
+})
